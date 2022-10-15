45d56039596542bef521edb1e0c882933a611ff4
Monika-patil11 <115860845+Monika-patil11@users.noreply.github.com>
2022-10-15 11:50:23 +0530

    Added conditional3.c

---
+++ conditional3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+  int age = 10;
+  
+ if(age >= 18)
+{
+  printf("You are eligible for voting!");
+
+}
+ else
+{
+ printf("You are not eligible for voting!");
+
+}
+
+}
